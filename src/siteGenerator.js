43c5be97a445f937a8527d160bb8a763b781ec0c
varun-gupta24 <varun.gupta24@trinityschoolnyc.org>
2023-08-21 12:58:02 -0400

    n

--- src/siteGenerator.js
+++ src/siteGenerator.js
@@ -5,6 +5,7 @@ const plotly = require('plotly');
 
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
 let projects_template = fs.readFileSync('views/projects.ejs', 'utf8');
+let research_template = fs.readFileSync('views/research.ejs', 'utf8');
 
 /*
   1) Generate an index page of all characters
@@ -17,8 +18,13 @@ let projects_html = ejs.render(projects_template, {
   filename: __dirname + '/views/projects.ejs'
 });
 
+let research_html = ejs.render(research_template, {
+  filename: __dirname + '/views/research.ejs'
+});
+
 
 fs.writeFileSync('../public/index.html', index_html, 'utf8');
 fs.writeFileSync('../public/projects.html', projects_html, 'utf8');
+fs.writeFileSync('../public/research.html', research_html, 'utf8');
 
 
